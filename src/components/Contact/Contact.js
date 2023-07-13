ba298ef0fc198842a497516d422c4f2f88321556
Gaetano Martello <gf.martello@gmail.com>
2023-07-13 11:08:28 -0400

    added styling to contact page

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,9 +5,9 @@ const Contact = () => {
   return (
     <section className='contact'>
       <h1>Contact</h1>
-      To book a session, please email me at <a href='mailto:kathymartello@gmail.com'>kathymartello@gmail.com</a>
+      To book a session, please email me at <a className='contact__link' href='mailto:kathymartello@gmail.com'>kathymartello@gmail.com</a>
       <br /> <br />
-      Or call at: <a href='tel:3474449637'>(347) 444-9637</a>
+      Or call at: <a className='contact__link' href='tel:3474449637'>(347) 444-9637</a>
     </section>
   );
 };
